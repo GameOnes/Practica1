965d9ebafc90536198db217494b1ca6e71ad9d66
GameOnes <97537553+GameOnes@users.noreply.github.com>
2022-04-27 20:22:12 +0200

    Primer apartado (png's)

--- practica1/Source/Game/Item.cs
+++ practica1/Source/Game/Item.cs
@@ -9,34 +9,63 @@ namespace TCGame
     }
     public class Bomb : Item
     {
-        Item texture=new Item()
+        Texture texture1 = new Texture("Data/Textures/Bomb.png");
+        public Bomb()
+        {
+            Texture = texture1;
+        }
     }
     public class Heart: Item
     {
-
+        Texture texture2 = new Texture("Data/Textures/Heart.png");
+        public Heart()
+        {
+            Texture = texture2;
+        }
     }
     public abstract class  Weapon: Item
     {
-
+        
     }
     public class Sword : Weapon
     {
-
+        Texture texture3 = new Texture("Data/Textures/Sword.png");
+        public Sword()
+        {
+            Texture = texture3;
+        }
+       
     }
     public class Axe : Weapon
     {
-
+        Texture texture4 = new Texture("Data/Textures/Axe.png");
+        public Axe()
+        {
+            Texture =texture4;
+        }
     }
     public class Coin: Item
     {
-
+        Texture texture5 = new Texture("Data/Textures/Coin.png");
+        public Coin()
+        {
+            Texture = texture5;
+        }
     }
     public class Clyde: Item
     {
-
+        Texture texture6 = new Texture("Data/Textures/Clyde.png");
+        public Clyde()
+        {
+            Texture = texture6;
+        }
     }
     public class Blinky : Item
     {
-
+        Texture texture7 = new Texture("Data/Textures/Blinky.png");
+        public Blinky()
+        {
+            Texture = texture7;
+        }
     }
 }
