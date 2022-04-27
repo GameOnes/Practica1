bae23f76f03156171c00c5a071545ea843cc5a0b
GameOnes <97537553+GameOnes@users.noreply.github.com>
2022-04-27 19:45:25 +0200

    .

--- practica1/Source/Game/Item.cs
+++ practica1/Source/Game/Item.cs
@@ -5,6 +5,38 @@ namespace TCGame
 {
     public abstract class Item : Sprite
     {
+        
+    }
+    public class Bomb : Item
+    {
+        Item texture=new Item()
+    }
+    public class Heart: Item
+    {
+
+    }
+    public abstract class  Weapon: Item
+    {
+
+    }
+    public class Sword : Weapon
+    {
+
+    }
+    public class Axe : Weapon
+    {
+
+    }
+    public class Coin: Item
+    {
+
+    }
+    public class Clyde: Item
+    {
+
+    }
+    public class Blinky : Item
+    {
 
     }
 }
